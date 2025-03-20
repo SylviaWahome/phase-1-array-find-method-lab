2f907fe9159f3f80b68bd8ebe192b89eece71ead
Shee-001 <sylviawanjiru804@gmail.com>
2025-03-20 10:40:02 +0300

    Implement superbowlWin function and tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+// Function to find the Superbowl win
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  // Export the function
+  module.exports = { superbowlWin };
